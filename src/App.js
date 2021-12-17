c00b0c4e474fc69a6c4d14bbb608bcb0f497717a
seongwoo <he1236@ajou.ac.kr>
2021-12-17 20:41:31 +0900

    React.createElement 연습

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import {hello} from "./component/hello";
+import {createElementPrac} from  "./component/createElementPrac"
 
 function App() {
   return (
     <div id="Main">
       {hello}
       {hello}
+      {createElementPrac}
       <img src={require("./cat.jpg")} alt="profile" />
     </div>
   )
---
+++ src/component/createElementPrac.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const createElementPrac = React.createElement(
+    'h1',
+    {className: 'greeting'},
+    'Hello, world!'
+);
