b4cc70f45662780e79edd4251192025d47f9ae61
seongwoo <he1236@ajou.ac.kr>
2021-12-18 11:52:50 +0900

    root DOM & ReactDOM.render() 설명 주석 첨부

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 
-ReactDOM.render(
-<App />,
-document.getElementById("root")
+ReactDOM.render( // 바로 이 ReactDOM.render( render 대상 element , document.getElementById('root')) 를 사용하여 element를 REACT ROOT DOM에 전달할 수 있다.
+<App />,// RENDER 대상 element
+document.getElementById("root") // root dom id - ./public/index.html 내부의 id='root'로 전달됨
 );
 
 // If you want to start measuring performance in your app, pass a function
